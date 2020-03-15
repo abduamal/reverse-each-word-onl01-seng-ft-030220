6e3ce51b89008b1958762fff333e627febe7a73c
Amal Abdu <abduamal@ymail.com>
2020-03-15 18:51:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,3 @@
-def reverse_each_word(string)
-  nustring = string.split(" ")
-  reversed = []
-  
-  nustring.each do |phrase|
-    reversed << phrase.reverse
-  end
-  
-  return reversed.join(" ")
-  
-end
 
 def reverse_each_word(sentence)
   
