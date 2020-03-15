7e74c8a3d14e74ce08ae0bbe81be319eb0c0d649
Amal Abdu <abduamal@ymail.com>
2020-03-15 18:51:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,7 @@
-
+  
 def reverse_each_word(sentence)
-  
-  nustring = string.split(" ")
-  nuarray = []
-  
-  nuarray = nustring.collect {|phrase| phrase.reverse}
-  return nuarray.join(" ")
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
 end
